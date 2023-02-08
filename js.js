d975cc194728858f2aa7d1ab2d183ce964de7465
RGALGH <120177378+RGALGH@users.noreply.github.com>
2023-02-08 10:55:27 +0100

    a

---
+++ js.js
@@ -0,0 +1,6 @@
+
+const listeMots = ['Suspect', 'Multiplication', 'Masquer', 'Manipuler', 'Consommer', 'Pair', 'Adhérer', 'Chariot', 'Crevette', 'Faim']
+
+function motAtrouver () {
+    let motCacher = Math.floor(Math.random())
+}
