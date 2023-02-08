ba2d75358d8f9523ff7ce1387d54e2381ab6d723
RGALGH <120177378+RGALGH@users.noreply.github.com>
2023-02-08 13:48:06 +0100

    a

--- js.js
+++ js.js
@@ -2,5 +2,12 @@
 const listeMots = ['Suspect', 'Multiplication', 'Masquer', 'Manipuler', 'Consommer', 'Pair', 'Adhérer', 'Chariot', 'Crevette', 'Faim']
 
 function motAtrouver () {
-    let motCacher = Math.floor(Math.random())
+    let motCacher = Math.floor(Math.random()* listeMots.length);
+    let motChoisi = listeMots[motCacher];
+    return motChoisi
+}
+
+function tableau (leMot) {
+    let tab = leMot.split("")
+    return tab
 }
